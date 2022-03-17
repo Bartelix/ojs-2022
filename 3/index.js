cd33b4e66d71e898c00845bd885a81476c098e79
Bartłomiej Bartoszewicz <bartlomiej.bartoszewicz95@gmail.com>
2022-03-17 22:50:23 +0100

    solution of task3

--- 3/index.js
+++ 3/index.js
@@ -1,12 +1,28 @@
-function magicTemplate() {
+function magicTemplate(strings, ...keys) {
+  const findPropValue = (prop, arr) => arr?.find((el) => el.hasOwnProperty(prop))?.[prop];
 
+  return (...values) => {
+    let dict = values[values.length - 1] || {};
+    let result = [strings[0]];
+    keys.forEach(function (key, i) {
+      let value;
+      if (typeof key === "object") {
+        const [k, v] = Object.entries(key)[0];
+        value = findPropValue(k, values) || v;
+      } else {
+        value = Number.isInteger(key) ? values[key] : dict[key];
+      }
+      result.push(value, strings[i + 1]);
+    });
+    return result.join("");
+  };
 }
 
-const firstName = 'James'
-const lastName = 'Bond'
+const firstName = "James";
+const lastName = "Bond";
 
-const update = magicTemplate`My name is ${{ lastName }}. ${{ firstName }} ${{ lastName }}.`
+const update = magicTemplate`My name is ${{ lastName }}. ${{ firstName }} ${{ lastName }}.`;
 
-update({ firstName: 'George' })
-update({ lastName: 'Weasley' })
-update({ firstName: 'Ron' })
+console.log(update({ firstName: "George" }));
+console.log(update({ lastName: "Weasley" }));
+console.log(update({ firstName: "Ron" }));
